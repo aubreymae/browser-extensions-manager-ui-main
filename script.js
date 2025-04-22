d3a2b535107cdeb51fd171e75fa9bcbd7271040d
aubreymae <aubreymaerg@gmail.com>
2025-04-22 16:43:18 -0400

    Updated code

--- script.js
+++ script.js
@@ -6,9 +6,21 @@ const extensionCard =
     .then((response) => response.json())
     .then((data) => {
       // Output data
+      for (let i = 0; i < data.length; i++) {
+        const card = document.createElement("article");
+        const extName = document.createElement("h4");
+        const extDesc = document.createElement("p");
 
-      console.log(data);
+        // Customize card component
+        card.className = "extension-card";
+
+        // Get extension name
+        extName.textContent = data[i].name;
+        extDesc.textContent = data[i].description;
+
+        card.appendChild(extName);
+        card.appendChild(extDesc);
+        container.appendChild(card);
+      }
     })
     .catch((error) => console.error("Failed to load JSON", error));
-
-console.log(data);
