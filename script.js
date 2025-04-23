9817f15999a786a1e9467cf9e411542fda8f0b61
aubreymae <aubreymaerg@gmail.com>
2025-04-23 16:42:01 -0400

    Added svg icons

--- script.js
+++ script.js
@@ -8,18 +8,30 @@ const extensionCard =
       // Output data
       for (let i = 0; i < data.length; i++) {
         const card = document.createElement("article");
+        const cardHeader = document.createElement("div");
+        const cardText = document.createElement("div");
+        const cardFooter = document.createElement("div");
         const extName = document.createElement("h4");
         const extDesc = document.createElement("p");
+        const icon = document.createElement("img");
 
         // Customize card component
         card.className = "extension-card";
+        cardHeader.className = "card-header";
+        icon.className = "icon";
 
         // Get extension name
         extName.textContent = data[i].name;
         extDesc.textContent = data[i].description;
 
-        card.appendChild(extName);
-        card.appendChild(extDesc);
+        icon.src = data[i].logo;
+
+        cardText.appendChild(extName);
+        cardText.appendChild(extDesc);
+        cardHeader.appendChild(icon);
+        cardHeader.appendChild(cardText);
+        card.appendChild(cardHeader);
+        card.appendChild(cardFooter);
         container.appendChild(card);
       }
     })
